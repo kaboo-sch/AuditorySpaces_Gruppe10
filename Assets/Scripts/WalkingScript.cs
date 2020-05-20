751ca6906a8561922dab4d05180d00d43aaf64a2
David <david.j.steiger@web.de>
2020-05-20 11:09:17 +0200

    Schrittsounds und erste Atmo für Wald eingefügt.

--- Assets/Scripts/WalkingScript.cs
+++ Assets/Scripts/WalkingScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WalkingScript : MonoBehaviour
 {
     public int speed = 5;
+    bool stepsFired = false;
+    public GameObject PlayerObj;
 
     // Update is called once per frame
     void Update()
@@ -12,6 +14,28 @@ public class WalkingScript : MonoBehaviour
         if (Input.GetButton("Fire1"))
         {
             transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;  //Position des Players += Kamerablickrichtung
+            startStepSound();
+            stepsFired = true;
         }
+
+        else if (stepsFired == true)
+        {
+            stopStepSound();
+        }
+    }
+
+    public void startStepSound()
+    {
+        if (stepsFired == false)
+        {
+            PlayerObj.GetComponent<AudioSource>().Play();
+        }
+    } 
+
+    public void stopStepSound()
+    {
+        PlayerObj.GetComponent<AudioSource>().Stop();
+        stepsFired = false;
+        Debug.Log("Stop Funktion");
     }
 }
