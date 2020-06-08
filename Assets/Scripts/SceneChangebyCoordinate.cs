385e17cd9a3a65f42d91d7574c025f2063d8cd5b
David <david.j.steiger@web.de>
2020-06-08 10:19:57 +0200

    Draußen- und Flur-Scene ausgebaut. Ton-Snippets eingefügt.

---
+++ Assets/Scripts/SceneChangebyCoordinate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangebyCoordinate : MonoBehaviour
+{
+    public GameObject playerObj;
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerObj.transform.position.x <= -24){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
