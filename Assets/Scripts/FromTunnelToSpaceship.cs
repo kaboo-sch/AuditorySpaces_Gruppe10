9e2f353f3421253b9a85ee018e4c50197a5c6a37
David <david.j.steiger@web.de>
2020-07-06 11:28:56 +0200

    Neuer Gang eingefügt. Texte unten verankert.

---
+++ Assets/Scripts/FromTunnelToSpaceship.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FromTunnelToSpaceship : MonoBehaviour
+{
+
+    bool WheelHit = false;
+    public GameObject myPlayer; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(WheelHit)
+        {
+            transform.Rotate(Vector3.right, 40f * Time.deltaTime);
+
+            if (myPlayer.GetComponent<GazeFiller>().imgGaze.fillAmount >= 1)
+            {
+                Debug.Log("Gaze Full");
+                SceneManager.LoadScene("SpaceStudio");
+            }
+        }
+
+
+    }
+
+    public void WheelIsHit()
+    {
+        WheelHit = true;
+
+    }
+
+    public void WheelIsUnhit()
+    {
+        WheelHit = false;
+    }
+}
