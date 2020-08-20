e97c8cdd2f5d66b2348f5398750e70048605b611
David <david.j.steiger@web.de>
2020-08-20 12:21:28 +0200

    Interaktion mit Auto. Neue Schritt-Geräusche in Stadt.

---
+++ Assets/Scripts/AutoLighter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLighter : MonoBehaviour
+{
+
+    private bool lighters;
+    public GameObject carMat;
+    private Rigidbody carBody;
+    public GameObject car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lighters = false;
+        carBody = car.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lighters){
+            carMat.GetComponent<Light>().enabled = true;
+        }
+
+        else {
+            carMat.GetComponent<Light>().enabled = false;
+        }
+    }
+
+    public void lightsOn(){
+        lighters = true;
+        carBody.AddForce(transform.up * 5000 * Time.deltaTime);
+    }
+
+    public void lightsOff(){
+        lighters = false;
+    }
+}
